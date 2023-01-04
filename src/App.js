c286306dd2a9bb6e93756458160c9c56ee166252
tmprova <applocker81@gmail.com>
2023-01-04 12:18:54 +0600

    daisy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">hello there</h1>
+    </div>
+  );
 }
 
 export default App;
